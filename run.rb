389dc083a8c0187405f7e1e0f38f554863bebe9f
Camilo Sanchez <camilin87@gmail.com>
2018-02-14 16:48:28 -0500

    validate the arguments

--- run.rb
+++ run.rb
@@ -1,10 +1,26 @@
 #!/usr/bin/env ruby
-p 'Tool to set the mp3 tag of all the files in a folder'
+
+DEBUG_LOG = ARGV.include?('--debug')
+
+def log_info(str)
+  puts str
+end
+
+def log_debug(str)
+  log_info(str) if DEBUG_LOG
+end
 
 input_folder = ARGV[0]
 author = ARGV[1]
 album = ARGV[2]
 
-p "input_folder=#{ARGV[0]}"
-p "author=#{ARGV[1]}"
-p "album=#{ARGV[2]}"
+log_debug 'Starting program...'
+
+log_debug "input_folder=#{ARGV[0]}"
+log_debug "author=#{ARGV[1]}"
+log_debug "album=#{ARGV[2]}"
+
+raise 'Input not found' unless Dir.exist?(input_folder)
+raise 'Invalid author' if author.empty?
+raise 'Invalid album' if album.empty?
+
