d6612d1952b17405bf015c69ca7a3009e09cf336
Camilo Sanchez <camilin87@gmail.com>
2018-02-14 16:58:24 -0500

    display the failed files exit the program with an error if at least one file failed to be processed

--- run.rb
+++ run.rb
@@ -24,8 +24,18 @@ raise 'Input not found' unless Dir.exist?(input_folder)
 raise 'Invalid author' if author.empty?
 raise 'Invalid album' if album.empty?
 
+should_fail = false
+
 Dir["#{input_folder}/*"]
   .select {|f| !File.directory? f}
   .each do |f|
-    log_debug "Processing #{f}"
+    begin
+      log_debug "Processing #{f}"
+    rescue Exception => e
+      log_debug e
+      log_info f
+      should_fail = true
+    end
   end
+
+raise 'Program Completed with errors' if should_fail
